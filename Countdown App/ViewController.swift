3120e230b987b38797a085e255a5cc01265832db
Lesslie Garcia <student@lgarcia1.local>
2017-01-09 11:42:20 -0600

    Commit Countdown App

--- Countdown App/ViewController.swift
+++ Countdown App/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var titleCountdownLabel: UILabel!
+    @IBOutlet weak var CountdownLabel: UILabel!
+    @IBOutlet weak var stopCountdownButton: UIButton!
+    var counter = 60
+    var timer = Timer()
+    var autonomous = true
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        displayCounter()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startCountdownButton(_ sender: Any) {
+        timer.invalidate()
+        counter = 60
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
     }
-
-
+    
+    func reset() {
+        counter = 60
+        displayCounter()
+    }
+    
+    func displayCounter() {
+        let minutes = counter / 60
+        let seconds = counter % 60
+        CountdownLabel.text = String(format: "%1d%:%02d%",minutes, seconds)
+    }
+    
+    func updateCounter() {
+        displayCounter()
+        counter -= 1
+        if counter < 0 {
+            reset()
+            
+        }
+        
+    }
+    
+    @IBAction func stopCountdownButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func resetCountdownButton(_ sender: Any) {
+        reset()
+    }
+    
+    
 }
-
